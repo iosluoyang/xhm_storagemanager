d1ac9d3fd0caa30bb6b16f9d12c9a7e437bf764f
HYOneMore <ioslhy@163.com>
2021-08-03 23:47:39 +0800

    update

--- uniCloud-aliyun/cloudfunctions/common/hello-common/index.js
+++ uniCloud-aliyun/cloudfunctions/common/hello-common/index.js
@@ -49,7 +49,7 @@ async function sendWxMiniMsg(param) {
 			/*
 			跳转小程序类型：developer为开发版；trial为体验版；formal为正式版；默认为正式版
 			*/
-			miniprogram_state: "developer",
+			miniprogram_state: "trial",
 			/*
 			进入小程序查看”的语言类型，支持zh_CN(简体中文)、en_US(英文)、zh_HK(繁体中文)、zh_TW(繁体中文)，默认为zh_CN
 			*/
