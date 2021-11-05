9c1953fd4ee46b19cca096fcc1ba31b9a68cf7d9
OneMore <ioslhy@163.com>
2021-11-05 21:11:59 +0800

    update

--- pages/wishlist/wishdraftpro.vue
+++ pages/wishlist/wishdraftpro.vue
@@ -11,6 +11,8 @@
 					collection="wish-draft-product, product" 
 					where=" creatUid == $cloudEnv_uid && status == 0 " 
 					orderby=" creatTime desc "
+					groupby=" sellerId"
+					group-field=" addToSet( pid ) as productList, addToSet( selectSpecPropInfo ) as selectSpecPropInfoList "
 					@load="loadData"
 		>
 			<view v-if="error" class="cu-load erro">{{ error.message }}</view>
@@ -21,27 +23,30 @@
 				<view class="storesview padding-sm">
 					
 					<view class="eachstoreview" v-for="(eachstore, storeindex) in data" :key="eachstore.sellerId">
+						
 						<!-- 商店头部展示区域 -->
-						<view class="storeheaderview cu-bar bg-white solids-bottom">
+						<view v-if="eachstore.sellerInfo" class="storeheaderview cu-bar bg-white solids-bottom">
 							<view class="action">
 								<text class="cuIcon cuIcon-shopfill text-pink"></text>
-								<text class="text-df text-black">{{ JSON.stringify(storeindex) }}</text>
+								<text class="text-df text-black">{{ eachstore.sellerInfo.nickName }}</text>
 							</view>
 						</view>
 						
 						<!-- 商店对应的商品列表 -->
 						<view class="storeproductlistview">
 							
-							<view class="eachproductview" v-for="(eachrecord, recordindex) in eachstore.recordList" :key="eachrecord._id">
+							<view class="eachproductview" v-for="(eachproduct, productindex) in eachstore.productList" :key="eachproduct._id">
 								
 								<!-- 商品基本信息 -->
 								<view class="productcontent flex align-center padding-sm">
-									<u-image class="flex0 margin-right" width="100" height="100" :src="eachrecord.product.imgs.split(',')[0]"></u-image>
-									<view class="titleview flex1 u-line-2" style="max-width: 200px;">{{ eachrecord.product.title }}</view>
+									<u-image class="flex0 margin-right" width="100" height="100" :src="eachproduct.imgs.split(',')[0]"></u-image>
+									<view class="titleview flex1 u-line-2" style="max-width: 200px;">{{ eachproduct.title }}</view>
 								</view>
 								
 								<!-- 选中的规格数组 -->
-								<view class="productspeclist">
+								<view v-if="eachproduct.selectSpecPropInfo" class="productspeclist padding-sm">
+									
+									
 									
 								</view>
 								
@@ -93,34 +98,30 @@
 			
 			// 加载数据 对数据进行加工
 			loadData(data, ended, pagination) {
-				
-				// 遍历所有数据按照店铺进行划分
-				let storeIds = []
-				let datalist = []
-				data.forEach(item => {
-					// 已经存在
-					if(storeIds.indexOf(item.sellerId) > -1) {
-						let storeitem = datalist.find(ownitem => (ownitem.sellerId == item.sellerId))
-						if(storeitem) {
-							storeitem.recordList = storeitem.recordList.concat(item)
-						}
-					}
-					// 不存在
-					else {
-						item['product'] = item.pid[0]
-						delete item.pid
-						let storeitem = {
-							sellerId: item.sellerId,
-							recordList: [item],
-							sellerInfo: item.product.sellerInfo
-						}
-						datalist.push(storeitem)
-						storeIds.push(item.sellerId)
-					}
-				})
-				
-				data = datalist
 				console.log(data);
+				
+				// 遍历数据进行数据重组
+				data.forEach(eachstore => {
+					
+					// 商店商品列表
+					let productList = []
+					let selectSpecPropInfoList = eachstore.selectSpecPropInfoList
+					
+					eachstore.productList.forEach((eachproduct, productindex) => {
+						eachproduct = eachproduct[0] // 转数组为对象
+						eachproduct['selectSpecPropInfo'] = selectSpecPropInfoList[productindex]
+						productList.push(eachproduct)
+						if(productindex == 0) {
+							eachstore['sellerInfo'] = eachproduct.sellerInfo
+						}
+					})
+					eachstore.productList = productList
+					
+					delete eachstore.selectSpecPropInfoList
+					
+				})
+				console.log(data);
+
 			},
 			
 		}
